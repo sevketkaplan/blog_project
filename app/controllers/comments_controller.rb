224a123b1b10ccfab93100b8ecf81afd0750fb5f
Sevket KAPLAN <sevketkaplan.20@gmail.com>
2018-01-17 01:34:35 +0300

    add comments delete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,11 @@ class CommentsController < ApplicationController
 		@comment=@post.comments.create(params[:comment].permit(:name, :body))
 		redirect_to post_path(@post)
 	end
+	def destroy
+		@post = Post.find(params[:post_id])
+		@comment @post.comments.find(rapams[:id])
+		@comment.destroy
+
+		redirect_to post_path(@post)
+	end
 end
